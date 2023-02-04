3ee5ba2e667bfe24d5790f1eb209c0701f26b983
ShafiaChy <shafiachy04@gmail.com>
2023-02-04 16:36:14 +0600

    try catch throw

---
+++ index.js
@@ -0,0 +1,19 @@
+function checkAge() {
+  const message = document.getElementById("show-error");
+  message.innerHTML = "";
+  let x = document.getElementById("age-field").value;
+  try {
+    if (x == "") {
+      throw "Empty";
+    } else if (x < 18) {
+      throw "Below 18";
+    } else if (x >= 18 && x <= 30) {
+      alert("Successfully submitted!");
+      document.getElementById("age-field").value = "";
+    } else if (x > 30) {
+      throw "Above 30";
+    }
+  } catch (err) {
+    message.innerHTML = "Age is " + err;
+  }
+}

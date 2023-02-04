4376d57ac38558ae620478050693817453f84ca8
ShafiaChy <shafiachy04@gmail.com>
2023-02-04 16:43:06 +0600

    code updated

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function checkAge() {
   let x = document.getElementById("age-field").value;
   try {
     if (x == "") {
-      throw "Empty";
+      throw "empty";
     } else if (x < 18) {
-      throw "Below 18";
+      throw "below 18";
     } else if (x >= 18 && x <= 30) {
       alert("Successfully submitted!");
       document.getElementById("age-field").value = "";
     } else if (x > 30) {
-      throw "Above 30";
+      throw "above 30";
     }
   } catch (err) {
     message.innerHTML = "Input is " + err;
